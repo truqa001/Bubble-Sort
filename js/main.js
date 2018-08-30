6df8984bc34585272ab6db7824510451a8510f04
Anthony <chumki@Anthonys-MacBook-Pro.local>
2018-08-30 18:45:04 +0930

    Implement bubble sort

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
+let array;
+let X;
+const WIDTH = 5;
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
+    frameRate(10);
 
-    const WIDTH = 20;
-    let X = 0;
+    //number of columns
+    let arraySize = parseInt(width/WIDTH);
+    array = new Array(arraySize);
 
-    let array = [];
-
-
-    let number_of_columns = parseInt(window.innerWidth/WIDTH);
-    print(number_of_columns);
-
-    for (var i=0; i<number_of_columns;i++){
-        let HEIGHT = random(100,window.innerHeight-100);
-        let block = rect(X,window.innerHeight-HEIGHT,WIDTH,HEIGHT);
-        array.push(block);
-        X += WIDTH;
+    //init heights for objects
+    for (var i=0;i<array.length;i++){
+        let HEIGHT = parseInt(random(100,height-100));
+        array[i] = HEIGHT;
     }
 
+    //init rect objects
+
 }
+
 function draw() {
+   /* xPos = xPos + 1;
+    line(xPos, 0 , xPos, height);*/
+    background(0);
+
+    X = 0;
+
+
+   for (var i=0;i<array.length;i++){
+
+       for (i; i<array.length;i++){
+           rect(X,height-array[i],WIDTH,array[i]);
+           X += WIDTH;
+       }
+
+       for (var j=0;j<array.length;j++){
+           if (array[j] > array[j+1]){
+               let temp = array[j];
+               array[j] = array[j+1];
+               array[j+1] = temp;
+           }
+       }
+
+    }
 
 }
 
@@ -26,3 +51,17 @@ function draw() {
 function randomHeight(){
     return random(100, window.outerHeight-100);
 }
+
+
+function bubble_sort(array){
+    for (var i=0;i<array.length;i++){
+        for (var j=0;j<array.length;j++){
+            if (array[j] > array[j+1]){
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+}
+
